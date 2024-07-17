497432cb2b9b8a9075a277a64e88ad0c6b955325
ilkbkr67 <ilkay.bakar@gmail.com>
2024-07-17 21:41:47 +0200

    ajout page connexion, inscription, mon compte

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,7 +4,10 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La galerie", "/pages/galerie.html"),
-    new Route("/connexion", "Connexion", "/pages/connexion.html"),
+    new Route("/connexion", "Connexion", "/pages/auth/connexion.html"),
+    new Route("/inscription", "Inscription", "/pages/auth/inscription.html"),
+    new Route("/compte", "Mon Compte", "/pages/auth/compte.html"),
+    new Route("/changementmdp", "Modifier votre mot de passe", "/pages/auth/changementmdp.html"),
 ];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 
